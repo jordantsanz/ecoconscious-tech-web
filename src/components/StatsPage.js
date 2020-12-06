252a4b50bf64cf8c2e90eb59ebd26e5034f5e450
jordantsanz <jordan.t.sanz.22@dartmouth.edu>
2020-12-06 10:23:20 -0600

    merge

--- src/components/StatsPage.js
+++ src/components/StatsPage.js
@@ -904,7 +904,7 @@ class StatsPage extends Component {
         return (
           <div className="top-result">
             <div className="website">{this.state.mainkey}</div>
-            <div className="is"><span className="bold-black">is</span></div>
+            <div className="is"><span className="bold-black-new">is</span></div>
             <div className="box green"><span className="flexspan">GREEN</span></div>
           </div>
         );
@@ -912,7 +912,7 @@ class StatsPage extends Component {
         return (
           <div className="top-result">
             <div className="website">{this.state.mainkey}</div>
-            <div className="is"><span className="bold-black">is</span></div>
+            <div className="is"><span className="bold-black-new">is</span></div>
             <div className="box notgreen"><span className="flexspan">NOT GREEN</span></div>
           </div>
         );
